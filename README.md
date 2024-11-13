f20368f7be5048919e93d0c5a1530abb86dc2997
cyanspaghetti <69961623+CyanSpaghetti@users.noreply.github.com>
2024-11-13 16:47:48 +0500

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+# CLI Wordle in Rust
+
+I'm learning rust okay forgive how bad my projects are
---
+++ main.rs
@@ -0,0 +1,49 @@
+use rand::prelude::*;
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    // choose a word from a random list of words
+    let word = get_word();
+    // take input
+    let mut attempt = get_attempt();
+    while compare_result(attempt, word.clone()) {
+        attempt = get_attempt();
+    }
+}
+
+fn compare_result(input : String, word : String) -> bool {
+    let mut n : usize = 0;
+    for _ in input.chars() {
+        if input.chars().nth(n).unwrap() == word.chars().nth(n).unwrap() { 
+            println!("C: {}", input.as_bytes()[n] as char);
+        } else { 
+            println!("W: *"); 
+        }
+        n+=1;
+    }
+
+    if input == word {
+        return false;
+    }
+    return true;
+}
+fn get_attempt() -> String {
+    let mut s : String = String::new();
+    print!("Enter Attempt: ");
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    s
+}
+
+fn get_word() -> String {
+    let possible_words = ["chess", "court", "count", "creek", "cunty"];
+    let mut rng = thread_rng();
+    let n: usize = rng.gen_range(0..possible_words.len());
+    String::from(possible_words[n]) // return the string
+}
